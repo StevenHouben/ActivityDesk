c37b8bd95e7659ba929697bc7db7a4efcf2e8d72
Steven Houben <shou@itu.dk>
2014-03-19 20:50:28 +0100

    Custom touch down event

--- ActivityDesk/Visualizer/Visualizations/VisualizationTablet.xaml.cs
+++ ActivityDesk/Visualizer/Visualizations/VisualizationTablet.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -151,6 +152,47 @@ namespace ActivityDesk.Visualizer.Visualizations
         public string ResourceType
         {
             get; set;
+        }
+
+        private void Grid_OnTouchDown(object sender, TouchEventArgs e)
+        {
+            if (IsDoubleTap(e))
+                OnDoubleTouchDown(sender);
+        }
+        private readonly Stopwatch _doubleTapStopwatch = new Stopwatch();
+        private Point _lastTapLocation;
+
+        public event EventHandler DoubleTouchDown;
+
+        protected virtual void OnDoubleTouchDown(object sender)
+        {
+            var fe = sender as FrameworkElement;
+            if (fe == null) return;
+
+            var frameworkElement = Parent as FrameworkElement;
+            if (frameworkElement == null) return;
+            var point = fe.TranslatePoint(new Point(0, 0), frameworkElement.Parent as FrameworkElement);
+
+            var res = fe.DataContext as LoadedResource;
+
+            if (res == null) return;
+
+            LoadedResources.Remove(res);
+
+            if (ResourceReleased != null)
+                ResourceReleased(res, point);
+
+            if (Resource == res)
+                Resource = LoadedResources.Count != 0 ? LoadedResources.First() : LoadedResource.EmptyResource;
+        }
+
+        private bool IsDoubleTap(TouchEventArgs e)
+        {
+            TimeSpan elapsed = _doubleTapStopwatch.Elapsed;
+            _doubleTapStopwatch.Restart();
+            bool tapsAreCloseInTime = (elapsed != TimeSpan.Zero && elapsed < TimeSpan.FromSeconds(0.7));
+
+            return tapsAreCloseInTime;
         }
     }
 }
